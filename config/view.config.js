e643313370411f41feed6e4d1a14b88cdf06d7b8
HandsomeWeeb5 <farhantb2000@gmail.com>
2022-06-28 22:02:59 +0700

    Update 2: Adding handlebars view (hbs) file

---
+++ config/view.config.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const handlebarsViewEngine = (app, view_engine) => {
+    express.static('public');
+
+    //* file ext: handlebars as hbs
+    app.engine('.hbs', view_engine);
+    app.set('view engine', '.hbs');
+    app.set('views', './public/views')
+}
+
+module.exports = {
+    handlebarsViewEngine: handlebarsViewEngine
+}
---
+++ server.js
@@ -0,0 +1,59 @@
+//* ________ SERVER.JS ______
+const express = require('express');
+const cors = require("cors");
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
+const handlebars = require('express-handlebars');
+
+// Import Modules
+const { handlebarsViewEngine } = require('./config/view.config');
+
+// Access env file untuk menjalankan sistem
+require('dotenv').config();
+
+const PORT = process.env.PORT || 7200
+
+const app = express();
+// Parsing (uraikan) data form
+app.use(express.urlencoded(
+    { extended: true }
+))
+// Parsing (uraikan) data JSON dari Database
+app.use(express.json())
+
+// aktifkan untuk perizinan CORS
+app.use(cors());
+
+// Pasang session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 // 864000000 1 day
+    }
+}));
+
+// Pasang cookie parser
+app.use(cookieParser('secret'));
+
+// Nyalakan pesan flash
+app.use(flash())
+
+// Pasang View Engine Handlebars
+handlebarsViewEngine(app, handlebars.engine({
+    extname: '.hbs',
+    layoutsDir: `${__dirname}/public/views/layouts`
+}));
+
+// Server listen to Port 7200
+app.listen(PORT, (error) => {
+    if (error) throw error;
+    console.log(`Server listening to http://localhost:${PORT}`);
+})
+
+app.get('/', (req, res) => {
+
+});
