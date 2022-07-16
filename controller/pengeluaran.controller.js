91609276f7e849d1b3e5f92201e3dfd480e083cc
HandsomeWeeb5 <farhantb2000@gmail.com>
2022-07-16 21:40:03 +0700

    Update 46: Add Dynamic Count Item in Cart

--- controller/pengeluaran.controller.js
+++ controller/pengeluaran.controller.js
@@ -2,12 +2,15 @@ const barangService = require('../services/barang.service');
 
 const viewDataCartTable = async(req, res) => {
     let results = await barangService.getDataFromCart();
+    let totalPrice = await barangService.countAllItem_in_Cart();
+    let total_harga = totalPrice[0].total_harga;
 
     try {
         res.render('pengeluaran', {
             layout: 'index',
             data: {
-                results
+                results,
+                total_harga
             }
         });
     } catch (error) {
--- services/barang.service.js
+++ services/barang.service.js
@@ -28,6 +28,17 @@ let sql__viewByFilter = `
     SELECT b.id_barang, b.deskripsi_brg, DATE_FORMAT(CONVERT_TZ(d.tgl_pemasukan, '+00:00', '+07:00'), "%d-%M-%Y") AS tgl_pemasukan, j.jenis_barang, b.waktu, d.no_dokumen_bc, b.merek_brg, b.harga_per_unit, b.vendor_item, b.hs_code, b.barcode_brg, b.img_barang FROM tb_barang AS b JOIN tb_dokumen AS d ON (b.id_dokumen = d.id_dokumen) JOIN tb_jenis_barang AS j ON (b.id_jenis_brg = j.id_jenis_brg) WHERE d.tgl_pemasukan = ? OR d.no_dokumen_bc = ? OR b.waktu = ? OR b.merek_brg = ? OR b.harga_per_unit = ? OR j.jenis_barang = ? ORDER BY b.id_barang; 
 `;
 
+let sql__viewDataCart = `SELECT 
+    id_keranjang,
+    deskripsi_barang,
+    DATE_FORMAT(CONVERT_TZ(tgl_pemasukan, '+00:00', '+07:00'), "%d-%M-%Y") AS tgl_pemasukan,
+    jenis_barang,
+    merek_barang,
+    no_dokumen_bc,
+    barcode_barang,
+    harga_per_unit
+    FROM tb_keranjang`;
+
 //TODO CALL ubahBarang: 
 // 1(?) => id_barang;
 // 2(?) => deskripsi_brg;
@@ -39,8 +50,6 @@ let sql__viewByFilter = `
 // 8(?) => hs_code;
 // 9(?) => barcode_barang;
 // 10(?) => gambar_barang;
-
-let sql__deleteByIds = `CALL hapusBarang(?)`;
 
 let sql__insertItem = `CALL tambahkanBarang(?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
@@ -132,12 +141,21 @@ const copyDataToDeliveryCart = (arrayString) => {
 
 const getDataFromCart = () => {
     return new Promise(async (resolve, reject) => {
-        database.query('SELECT * FROM tb_keranjang', (err, result) => {
+        database.query(sql__viewDataCart, (err, result) => {
             if(err) throw err;
             resolve(result);
         });
     })
 }
+
+const countAllItem_in_Cart = () => {
+    return new Promise(async (resolve, reject) => {
+        database.query('SELECT SUM(harga_per_unit) AS total_harga FROM tb_keranjang;', (err, result) => {
+            if(err) throw err;
+            resolve(result);
+        });
+    })
+};
 
 module.exports = {
     //getDataPerPage: getDataPerPage,
@@ -150,7 +168,8 @@ module.exports = {
     totalHargaDataBarang: totalHargaDataBarang,
     countAllItem_in_Table: countAllItem_in_Table,
     //TODO Services for Pengeluaran
-    getDataFromCart: getDataFromCart
+    getDataFromCart: getDataFromCart,
+    countAllItem_in_Cart: countAllItem_in_Cart
 }
 
 /*
